5265765c1d4af737cde9fb35b20b2b6cad29dd77
omar elsawy <omar.elsawy012@gmail.com>
2023-02-16 09:54:45 +0200

    order show route

--- routes/order.js
+++ routes/order.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/', orderController.create);
 router.get('/', orderController.all);
+router.get('/:id', orderController.show);
 
 module.exports = router;
