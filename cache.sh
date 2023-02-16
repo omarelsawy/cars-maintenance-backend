b3640d5a877f96c60f3a22edb649bc0e53c8fee9
omar elsawy <omar.elsawy012@gmail.com>
2023-02-16 14:40:55 +0200

    remove cache

--- cache.sh
+++
@@ -1,9 +0,0 @@
-if [[ -d "$XDG_CACHE_HOME"/public ]]; then
-    echo "found cached public dir"
-else
-    echo "No cached public dir found"
-fi
-
-npm install
-    
-rsync -a "$XDG_CACHE_HOME"/public/images/ public/images
